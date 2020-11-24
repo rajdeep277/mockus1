d3d0a0a9b712f1861780ee804cd9ff9c557c885e
rajdeep277 <74271831+rajdeep277@users.noreply.github.com>
2020-11-24 13:26:00 -0800

    Add files via upload

---
+++ dev/scripts/script.js
@@ -0,0 +1,9 @@
+// Write your JavaScript here...
+function openNav(){
+	var x = document.getElementById("nav-bar");
+	if (x.style.display === "none") {
+		x.style.display = "block";
+	}else{
+		x.style.display = "none";
+	}
+}
